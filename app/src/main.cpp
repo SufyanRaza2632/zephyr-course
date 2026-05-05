8ddeee641239ce5e89f6f334437852f2ec8c4eb2
Muhammad Sufyan Raza <msufyanraza.26@gmail.com>
2026-05-05 14:56:57 +0200

    Placed the boards folder in Project-root dir.Updated main.c and dts files for custom board

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -5,7 +5,7 @@
 
 /* The devicetree node identifier for the "led0" alias. using DT_ALIAS */
 #define LED_NODE DT_ALIAS(app_led)
-
+#define LED_NODE_TEST DT_ALIAS(led0)
 /* The devicetree node identifier for the "led0" alias. using DT_NODELABEL*/
 //#define LED_NODE DT_NODELABEL(heartbeat_led)
 
@@ -13,21 +13,31 @@
 //#define LED_NODE DT_PATH(leds, led_4)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct gpio_dt_spec led_test = GPIO_DT_SPEC_GET(LED_NODE_TEST, gpios);
 
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
     bool led_state = true;
+    bool led_test_state = false;
 
     if (!gpio_is_ready_dt(&led)) return 0;
 
+    if (!gpio_is_ready_dt(&led_test)) return 0;
+
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+
+    if (gpio_pin_configure_dt(&led_test, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+    if(gpio_pin_set_dt(&led_test,0 ) < 0 ) return 0;
 
     while (1) {
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
+        if (gpio_pin_toggle_dt(&led_test) < 0) return 0;
 
         led_state = !led_state;
+        led_test_state = !led_test_state;
+        
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }

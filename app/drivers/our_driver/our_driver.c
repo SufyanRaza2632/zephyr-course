a336afab96684fff3409bf792e6e7978fc031c66
Muhammad Sufyan Raza <msufyanraza.26@gmail.com>
2026-05-13 00:47:52 +0200

    Created a driver from scratch and implement two APIs for turning led on and off

---
+++ app/drivers/our_driver/our_driver.c
@@ -0,0 +1,41 @@
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+
+#define DT_DRV_COMPAT  our_driver
+#define LED_NODE_DRIV DT_ALIAS(led3)
+LOG_MODULE_REGISTER(our_driver,LOG_LEVEL_INF);
+
+static const struct gpio_dt_spec led_driv = GPIO_DT_SPEC_GET(LED_NODE_DRIV, gpios);
+static int sample_fetch_impl(const struct device *dev,
+				     enum sensor_channel chan)
+{
+    LOG_INF("Hello from channel get, channel %d", chan);
+    if(gpio_pin_set_dt(&led_driv,0 ) < 0 ) return 0;
+    return 0;
+}
+static int channel_get_impl(const struct device *dev,
+				    enum sensor_channel chan,
+				    struct sensor_value *val)
+{
+
+    LOG_INF("Hello from channel get, channel %d", chan);
+     if(gpio_pin_set_dt(&led_driv,1 ) < 0 ) return 0;
+    return 0;
+}
+        
+
+static DEVICE_API(sensor, api_iomico) = {
+    .channel_get  = channel_get_impl,
+    .sample_fetch = sample_fetch_impl,
+};
+
+ 
+
+static int init(const struct device *dev)
+{
+ if (!gpio_is_ready_dt(&led_driv)) return 0;
+ if (gpio_pin_configure_dt(&led_driv, GPIO_OUTPUT_ACTIVE) < 0) return 0;
+ return 0;
+}
+ DEVICE_DT_INST_DEFINE(0  ,init,NULL,NULL,NULL, POST_KERNEL,80, &api_iomico );
--- boards/custom_board_from_scratch/board.cmake
+++ boards/custom_board_from_scratch/board.cmake
@@ -4,8 +4,10 @@ message(STATUS "CUSTOM BOARD.CMAKE LOADED")
 # keep first
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 board_runner_args(jlink "--device=STM32F407VG" "--speed=4000")
-
+board_runner_args(pyocd "--device=STM32F407VG")
 # keep first
+
+include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd-stm32.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)

3b185aa48c0407ad55f5dbe45e52bc22e7cc3f7a
Muhammad Sufyan Raza <msufyanraza.26@gmail.com>
2026-05-22 01:29:38 +0200

    Expose the extension API as a shell subcommand

--- app/drivers/our_driver/our_driver_custom_api.h
+++ app/drivers/our_driver/our_driver_custom_api.h
@@ -5,7 +5,7 @@
 extern "C" {
 #endif
 int our_driver_custom_data_update(const struct device *dev, int value);
-
+int our_driver_custom_data_read(const struct device *dev);
 #ifdef __cplusplus
 }
 #endif
